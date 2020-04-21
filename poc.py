1899d1712f08497bb4f85cc1da6aedb22b06cbca
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-04-21 18:39:18 +0200

    Update poc

--- poc.py
+++ poc.py
@@ -17,7 +17,6 @@ box = pv.Box(bounds=(0, 1.0, 0, 1.0, 0, 1.0))
 box_points = box.points.copy()
 box_offset = (-.5, -.5, 0)
 
-
 # plotter
 plotter = pv.BackgroundPlotter(menu_bar=False, toolbar=False)
 plotter.enable_anti_aliasing()
@@ -27,37 +26,62 @@ box_actor = plotter.add_mesh(box, show_edges=True, color='tan', opacity=0.7)
 box_actor.VisibilityOff()
 
 
-def on_pick(vtk_picker, event):
-    cell_id = vtk_picker.GetCellId()
-    mesh = vtk_picker.GetDataSet()
-    if cell_id != -1:
-        if hasattr(mesh, "_name") and mesh._name == "GRID":
-            indices = vtk.vtkIdList()
-            mesh.GetCellPoints(cell_id, indices)
-            vertices = [mesh.GetPoint(indices.GetId(i)) for i in range(4)]
-            center = np.mean(vertices, axis=0)
-            box.points = box_points.copy() + box_offset
-            box.translate(center)
-            plotter.update()
-            box_actor.VisibilityOn()
-    else:
-        box_actor.VisibilityOff()
+class Builder(object):
+    def __init__(self, plotter):
+        self.plotter = plotter
+        self.button_pressed = False
+        self.box_transform = (0, 0, 0)
+
+        iren = self.plotter.iren
+        iren.AddObserver(
+            vtk.vtkCommand.MouseMoveEvent,
+            self.on_mouse_move
+        )
+        iren.AddObserver(
+            vtk.vtkCommand.LeftButtonPressEvent,
+            self.on_button_press
+        )
+        iren.AddObserver(
+            vtk.vtkCommand.EndInteractionEvent,
+            self.on_button_release
+        )
+
+        self.picker = vtk.vtkCellPicker()
+        self.picker.AddObserver(
+            vtk.vtkCommand.EndPickEvent,
+            self.on_pick
+        )
+
+    def on_mouse_move(self, vtk_picker, event):
+        x, y = vtk_picker.GetEventPosition()
+        self.picker.Pick(x, y, 0, plotter.renderer)
+
+    def on_button_press(self, vtk_picker, event):
+        self.button_pressed = True
+
+    def on_button_release(self, vtk_picker, event):
+        if self.button_pressed:
+            fixed_box = pv.Box(bounds=(0, 1.0, 0, 1.0, 0, 1.0))
+            fixed_box.translate(self.box_transform)
+            plotter.add_mesh(fixed_box, show_edges=True, color='tan')
+        self.button_pressed = False
+
+    def on_pick(self, vtk_picker, event):
+        cell_id = vtk_picker.GetCellId()
+        mesh = vtk_picker.GetDataSet()
+        if cell_id != -1:
+            if hasattr(mesh, "_name") and mesh._name == "GRID":
+                indices = vtk.vtkIdList()
+                mesh.GetCellPoints(cell_id, indices)
+                vertices = [mesh.GetPoint(indices.GetId(i)) for i in range(4)]
+                center = np.mean(vertices, axis=0)
+                self.box_transform = center + box_offset
+                box.points = box_points.copy()
+                box.translate(self.box_transform)
+                plotter.update()
+                box_actor.VisibilityOn()
+        else:
+            box_actor.VisibilityOff()
 
 
-picker = vtk.vtkCellPicker()
-picker.AddObserver(
-    vtk.vtkCommand.EndPickEvent,
-    on_pick
-)
-
-
-def on_mouse_move(vtk_picker, event):
-    x, y = vtk_picker.GetEventPosition()
-    picker.Pick(x, y, 0, plotter.renderer)
-
-
-iren = plotter.iren
-iren.AddObserver(
-    vtk.vtkCommand.MouseMoveEvent,
-    on_mouse_move
-)
+builder = Builder(plotter)
