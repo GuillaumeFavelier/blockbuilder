f3d872cd93fe481571912ce729dfc2b5e344f594
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-04-22 19:02:25 +0200

    Update poc

--- poc.py
+++ poc.py
@@ -2,59 +2,81 @@ import numpy as np
 import pyvista as pv
 import vtk
 
-# background
-background_color1 = (0.05, 0.05, 0.05)
-background_color2 = (0., 0., .35)
+
+# default params
+rcParams = {
+    "background_color_top": (0.05, 0.05, 0.05),
+    "background_color_bottom": (0., 0., .35),
+    "plane_color": (.3, .3, .3),
+    "grid_color": (.1, .1, .7),
+    "selector_color": (.1, .1, .7),
+    "block_color": (1., 1., 1.),
+}
+
+#################
 
 # plane
-plane_color = (.3, .3, .3)
 plane_size = 20
-plane = pv.Plane(center=(5, 5, -0.0001),
+plane = pv.Plane(center=(5, 5, -1.),
                  i_size=plane_size, j_size=plane_size,
                  i_resolution=1, j_resolution=1)
 
 # grid
-grid_color = (.1, .1, .7)
 grid_size = 10
 grid_length = (grid_size - 1) ** 2
 grid = pv.UniformGrid()
 grid.dimensions = (grid_size, grid_size, 1)
 
-# box
-box = pv.Box(bounds=(0, 1.0, 0, 1.0, 0, 1.0))
-box_points = box.points.copy()
-box_offset = (-.5, -.5, 0)
-box_color = (.1, .1, .7)
-box_scalars = np.tile(box_color, (6, 1))
-box.cell_arrays["color"] = box_scalars
+# selector
+selector = pv.Box(bounds=(0, 1.0, 0, 1.0, 0, 1.0))
+selector_points = selector.points.copy()
+selector_offset = (-.5, -.5, 0)
+selector_scalars = np.tile(rcParams["selector_color"], (6, 1))
+selector.cell_arrays["color"] = selector_scalars
 
 # plotter
 plotter = pv.BackgroundPlotter(menu_bar=False, toolbar=False)
-plotter.set_background(background_color2, top=background_color1)
+plotter.set_background(
+    color=rcParams["background_color_bottom"],
+    top=rcParams["background_color_top"],
+)
 plotter.enable_anti_aliasing()
 plotter._key_press_event_callbacks.clear()
 plotter._style = vtk.vtkInteractorStyleUser()
 plotter.update_style()
 
-# plane_actor = plotter.add_mesh(plane, show_edges=True, color=plane_color)
-# plane_actor._mesh = plane
-# plane_actor._name = "plane"
-grid_actor = plotter.add_mesh(grid, show_edges=True, color=grid_color,
-                              opacity=0.7)
+plane_actor = plotter.add_mesh(
+    plane,
+    show_edges=True,
+    color=rcParams["plane_color"],
+)
+plane_actor._mesh = plane
+plane_actor._name = "plane"
+grid_actor = plotter.add_mesh(
+    grid,
+    show_edges=True,
+    color=rcParams["grid_color"],
+    opacity=0.7
+)
 grid_actor._mesh = grid
 grid_actor._name = "grid"
-box_actor = plotter.add_mesh(box, show_edges=True, scalars="color",
-                             rgb=True, opacity=0.7)
-box_actor._mesh = box
-box_actor._name = "selector"
-box_actor.VisibilityOff()
+selector_actor = plotter.add_mesh(
+    selector,
+    show_edges=True,
+    scalars="color",
+    rgb=True,
+    opacity=0.7
+)
+selector_actor._mesh = selector
+selector_actor._name = "selector"
+selector_actor.VisibilityOff()
 
 
 class Builder(object):
     def __init__(self, plotter):
         self.plotter = plotter
         self.button_pressed = False
-        self.box_transform = (0, 0, 0)
+        self.selector_transform = (0, 0, 0)
         self.grid_zpos = 0
 
         iren = self.plotter.iren
@@ -123,41 +145,54 @@ class Builder(object):
 
             if intersections["grid"]:
                 grid_data = intersections["grid"]
-                if grid_data[0] == 0:
-                    point = points.GetPoint(grid_data[0])
-                    pt_min = np.floor(point).astype(np.int)
-                    pt_min[-1] = self.grid_zpos
-                    center = pt_min + [0.5, 0.5, 0]
-                    self.box_transform = center + box_offset
-                    box.points = box_points.copy()
-                    box.translate(self.box_transform)
-                    box_actor.VisibilityOn()
+                # draw the selector
+                point = points.GetPoint(grid_data[0])
+                pt_min = np.floor(point).astype(np.int)
+                pt_min[-1] = self.grid_zpos
+                center = pt_min + [0.5, 0.5, 0]
+                self.selector_transform = center + selector_offset
+                selector.points = selector_points.copy()
+                selector.translate(self.selector_transform)
+                selector_actor.VisibilityOn()
+                add_block = True
+                if intersections["block"]:
+                    block_data = intersections["block"]
+                    if block_data[1]._zpos == self.grid_zpos:
+                        add_block = False
+
+                if add_block:
                     if self.button_pressed:
-                        fixed_box = pv.Box(bounds=(0, 1.0, 0, 1.0, 0, 1.0))
-                        fixed_box_color = (1., 1., 1.)
-                        fixed_box_scalars = np.tile(fixed_box_color, (6, 1))
-                        fixed_box.cell_arrays["color"] = fixed_box_scalars
-                        fixed_box.translate(self.box_transform)
-                        fixed_box_actor = plotter.add_mesh(
-                            fixed_box, show_edges=True, reset_camera=False,
+                        block = pv.Box(bounds=(0, 1.0, 0, 1.0, 0, 1.0))
+                        block_scalars = np.tile(
+                            rcParams["block_color"],
+                            (6, 1)
+                        )
+                        block.cell_arrays["color"] = block_scalars
+                        block.translate(self.selector_transform)
+                        block_actor = plotter.add_mesh(
+                            block, show_edges=True, reset_camera=False,
                             scalars="color", rgb=True
                         )
-                        fixed_box._transform = self.box_transform
-                        fixed_box_actor._mesh = fixed_box
-                        fixed_box_actor._name = "block"
+                        block._transform = self.selector_transform
+                        block_actor._mesh = block
+                        block_actor._name = "block"
+                        block_actor._zpos = self.grid_zpos
                         self.button_released = False
-                    plotter.update()
+                plotter.update()
+            elif intersections["plane"]:
+                selector_actor.VisibilityOff()
+
             if intersections["block"]:
                 block_data = intersections["block"]
-                if block_data[0] == 0:
+                if block_data[0] == 1:
                     mesh = block_data[1]._mesh
-                    self.box_transform = mesh._transform
-                    box.points = box_points.copy()
-                    box.translate(self.box_transform)
-                    box_actor.VisibilityOn()
+                    self.selector_transform = mesh._transform
+                    selector.points = selector_points.copy()
+                    selector.translate(self.selector_transform)
+                    selector_actor.VisibilityOn()
                     plotter.update()
         else:
-            box_actor.VisibilityOff()
+            selector_actor.VisibilityOff()
 
 
 builder = Builder(plotter)
