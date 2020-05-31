b6361a9096460174e781a6241fc6b530a1026a7c
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-05-31 12:06:56 +0200

    Refactor Intersection, __init__ and adds requirements for testing

--- blockbuilder/__init__.py
+++ blockbuilder/__init__.py
@@ -1,8 +1,5 @@
 """Application for block building."""
 
-from .params import rcParams
-from .elements import Grid, Plane, SymmetrySelector, Selector, Block
 from .builder import Builder
-from .app import start
 
 __version__ = '0.4.dev0'
--- blockbuilder/builder.py
+++ blockbuilder/builder.py
@@ -8,6 +8,7 @@ import vtk
 from .params import rcParams
 from .plotter import Plotter
 from .elements import Element, Symmetry, SymmetrySelector, Grid, Plane, Block
+from .intersection import Intersection
 
 from PyQt5 import QtCore
 from PyQt5.Qt import QIcon, QSize
@@ -464,33 +465,6 @@ class Builder(Plotter):
         """Toggle area selection."""
         self.block.toggle_edges(value)
         self.render()
-
-
-class Intersection(object):
-    """Manage the intersections."""
-
-    def __init__(self, picker):
-        """Initialize the Intersection manager."""
-        self.any_intersection = (picker.GetCellId() != -1)
-        if self.any_intersection:
-            self.intersections = [None for element in Element]
-            self.picked_points = picker.GetPickedPositions()
-            self.picked_actors = picker.GetActors()
-            for idx, actor in enumerate(self.picked_actors):
-                self.intersections[actor.element_id.value] = idx
-
-    def exist(self):
-        """Return True is there is any intersection."""
-        return self.any_intersection
-
-    def element(self, element_id):
-        """Return True is there is any intersection with element_id."""
-        return self.intersections[element_id.value] is not None
-
-    def point(self, element_id):
-        """Return the intersection point of element_id."""
-        idx = self.intersections[element_id.value]
-        return np.asarray(self.picked_points.GetPoint(idx))
 
 
 def _get_toolbar_area(area):
