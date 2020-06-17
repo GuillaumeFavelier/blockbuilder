2859c3b0da5d2a6eec8189a9105df780335686fb
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-06-17 20:51:49 +0200

    Fix style

--- blockbuilder/help.py
+++ blockbuilder/help.py
@@ -7,4 +7,7 @@ class HelpDialog(QDialog):
     """Display basic help."""
 
     def __init__(self, parent=None):
+        """Initialize the HelpDialog."""
         super().__init__(parent)
+        self.setWindowTitle("Help")
+        self.setModal(True)
